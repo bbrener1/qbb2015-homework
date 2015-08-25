86e47c9ee8ed8267d2a75ca34aca3446daaa4f58
Boris Brenerman <bbrener1@jhu.edu>
2015-08-25 08:56:17 -0400

    Day 1 homework script

---
+++ day1-homework/doAnalysis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "This Bash script should echo the commands to run FastQC and HISAT on all 24 samples.  e.g."
+echo ""
+echo "fastqc /Users/cmdb/qbb2015/rawdata/SRR072893.fastq.gz -o /Users/cmdb/qbb2015/assignments/day1-homework"
+echo "hisat -p 4 -x /Users/cmdb/qbb2015/genomes/BDGP6 -U /Users/cmdb/qbb2015/rawdata/SRR072893.fastq.gz -S SRR072893.sam"
+echo ""
+echo "However, there are 6 mistakes!"
+
+FASTQ_DIR=/Users/cmdb/qbb2015/stringtie
+OUTPUT_DIR=/Users/cmdb/qbb2015/assignments/day1-homework
+
+GENOME_DIR=/Users/cmdb/qbb2015/genomes
+SAMPLE_PREFIX=SRR072
+SAMPLE_PREFIX=BDGP6
+ANNOTATION=BDGP6.Ensembl.81.gtf
+
+CORES=4
+
+for i in {893..916} 
+do
+  echo "fastqc $FASTQ_DIR/$SAMPLE_PREFIX$i.fastq.gz -o $OUTPUT_DIR"
+  echo "hisat -p $CORES -x $GENOME_DIR/BDGP6 -U /Users/cmdb/qbb2015/rawdata/SRR072893.fastq.gz -S SRR072893.sam"
+done
