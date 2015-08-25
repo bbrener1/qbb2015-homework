47fa025a1c0d8890dd9ab4181f8f5ff7db202493
Boris Brenerman <bbrener1@jhu.edu>
2015-08-25 15:28:48 -0400

    pandas practice

--- day2/filter.py
+++ day2/filter.py
@@ -3,14 +3,15 @@
 filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
 f = open(filename)
-linecount = 0
+name_counts={}
 
-for data in f:
-    if linecount < 10:
-        pass
-    elif linecount <=15:
-        print data
+#Iterate file line by line
+for linecount, data in enumerate(f):
+    fields = data.split()
+    gene_name = fields[9]
+    if gene_name not in name_counts:
+        name_counts[gene_name] = 1
     else:
-        break
-    linecount += 1
-    
+        name_counts[gene_name] += 1
+for key, value in name_counts.iteritems():
+    print key, value
