e65d77e07749db2ceecba4ab49068ef093df2992
Boris Brenerman <bbrener1@jhu.edu>
2015-08-26 10:19:56 -0400

    day 3 begin

---
+++ day3/filter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
+
+import sys
+
+#print sys.argv
+
+#filename = sys.argv[1]
+
+#f = open(filename)
+
+f = sys.stdin
+
+name_counts={}
+
+#Iterate file line by line
+for linecount, data in enumerate(f):
+    fields = data.split()
+    gene_name = fields[9]
+    if gene_name not in name_counts:
+        name_counts[gene_name] = 1
+    else:
+        name_counts[gene_name] += 1
+for key, value in name_counts.iteritems():
+    print key, value
